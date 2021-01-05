bcfe80a563b7fec11b06aee3fdced14c00701b8e
MUHAMMED AYIMEN ABDUL LATHEEF <aiymenlatheef03@gmail.com>
2021-01-05 23:24:32 +0530

    Update sparksfoundation.sql

--- SQL/sparksfoundation.sql
+++ SQL/sparksfoundation.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 05, 2021 at 08:57 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.0
+-- Host: localhost:3306
+-- Generation Time: Jan 05, 2021 at 05:49 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -28,7 +29,6 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `sparkstransact` (
-  `sno` int(3) NOT NULL,
   `sender` text NOT NULL,
   `receiver` text NOT NULL,
   `balance` int(8) NOT NULL,
@@ -39,8 +39,8 @@ CREATE TABLE `sparkstransact` (
 -- Dumping data for table `sparkstransact`
 --
 
-INSERT INTO `sparkstransact` (`sno`, `sender`, `receiver`, `balance`, `datetime`) VALUES
-(1, 'AYIMEN', 'BINURAJ', 1000, '2021-01-04 23:38:15');
+INSERT INTO `sparkstransact` (`sender`, `receiver`, `balance`, `datetime`) VALUES
+('AYIMEN', 'POOJA', 2000, '2021-01-05 16:51:04');
 
 -- --------------------------------------------------------
 
@@ -61,16 +61,36 @@ CREATE TABLE `sparksusers` (
 --
 
 INSERT INTO `sparksusers` (`id`, `name`, `email`, `phone`, `balance`) VALUES
-(10111, 'AYIMEN', 'aiymen234@gmail.com', 563454321, 4000),
-(10112, 'BINURAJ', 'binu345@gmail.com', 946784322, 7000),
-(10113, 'ADITHYA', 'adithya@gmail.com', 946324678, 3000),
-(10114, 'POOJA', 'poojar@gmail.com', 676567543, 5000),
+(10111, 'AYIMEN', 'aiymen234@gmail.com', 563454321, 2000),
+(10112, 'BINURAJ', 'binu345@gmail.com', 946784322, 5000),
+(10113, 'ADITHYA', 'adithya@gmail.com', 946324678, 4000),
+(10114, 'POOJA', 'poojar@gmail.com', 676567543, 7000),
 (10115, 'LEYA', 'leya123@gmail.com', 568228617, 2000),
 (10116, 'FAHIM', 'fahim@gmail.com', 456465678, 6000),
 (10117, 'SIDHARTH', 'sidharth@gmail.com', 345678435, 7000),
 (10118, 'KAILAS', 'kailas33@gmail.com', 676548127, 5000),
-(10119, 'NAVEEN', 'navz12@gmail.com', 567448971, 1000),
+(10119, 'NAVEEN', 'navz12@gmail.com', 567448971, 2000),
 (10120, 'VIPIN', 'vipin11@gmail.com', 600334561, 9000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `sparksusers`
+--
+ALTER TABLE `sparksusers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `sparksusers`
+--
+ALTER TABLE `sparksusers`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10122;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
